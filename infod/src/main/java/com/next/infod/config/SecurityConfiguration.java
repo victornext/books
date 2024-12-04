337718e5678d2cc4f38f37c53645867c499cdcb7
victornext <victorbrs517@gmail.com>
2024-12-04 01:52:22 -0300

    Atualização de security

--- infod/src/main/java/com/next/infod/config/SecurityConfiguration.java
+++ infod/src/main/java/com/next/infod/config/SecurityConfiguration.java
@@ -7,7 +7,15 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.beans.Encoder;
 
 
 @Configuration
@@ -29,4 +37,28 @@ public class SecurityConfiguration {
 
     }
 
+    @Bean
+    public PasswordEncoder  passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+            UserDetails user1 = User.builder()
+                    .username("usuario1")
+                    .password(encoder.encode("123"))
+                    .roles("USER") //Sempre caixa alta
+                    .build();
+
+
+        UserDetails user2 = User.builder()
+                .username("usuario2")
+                .password(encoder.encode("321"))
+                .roles("ADMIN") //Sempre caixa alta
+                .build();
+
+
+            return  new InMemoryUserDetailsManager(user1, user2);
+    }
 }
