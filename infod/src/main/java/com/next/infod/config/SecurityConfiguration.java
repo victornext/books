662178a9b83d2cd32adae5843febbbe653b64e10
victornext <victorbrs517@gmail.com>
2024-12-18 00:22:55 -0300

    a

--- infod/src/main/java/com/next/infod/config/SecurityConfiguration.java
+++ infod/src/main/java/com/next/infod/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.next.infod.config;
 
-
 import com.next.infod.security.CustomUserDetailsService;
 import com.next.infod.services.UsuarioService;
 import org.springframework.context.annotation.Bean;
@@ -12,16 +11,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.beans.Encoder;
-
 
 @Configuration
 @EnableWebSecurity
@@ -31,30 +25,28 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable)//Para aplicações web
-                .formLogin(Customizer.withDefaults()) //Para ela receber autenticação via login
+                .csrf(AbstractHttpConfigurer::disable) // Para aplicações web
+                .formLogin(Customizer.withDefaults()) // Para ela receber autenticação via login
                 .formLogin(configurer ->
                         configurer.loginPage("/login").
-                                permitAll()) //Formulario padrao
+                                permitAll()) // Formulário padrão
                 .httpBasic(Customizer.withDefaults()) //
-                .authorizeHttpRequests(authorizer ->{
+                .authorizeHttpRequests(authorizer -> {
                     authorizer.requestMatchers("/login/**").permitAll();
                     authorizer.requestMatchers(HttpMethod.POST, "/usuarios/**").permitAll();
-
-                    authorizer.anyRequest().authenticated();  //Para toda requisicao nessa API tem que estar autenticado
+                    authorizer.anyRequest().authenticated(); // Para toda requisição nessa API tem que estar autenticado
                 })
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults()) // OAuth2 login
                 .build();
-
     }
 
     @Bean
-    public PasswordEncoder  passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
     }
 
     @Bean
-    public GrantedAuthorityDefaults grantedAuthorityDefaults(){
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
         return new GrantedAuthorityDefaults("");
     }
 
