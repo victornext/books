52765e644758a4288b577ff8d0ea819e487ff31c
victornext <victorbrs517@gmail.com>
2024-12-25 01:36:28 -0300

    Usuarios que só tem login google

--- infod/src/main/java/com/next/infod/security/LoginSocialSucessHandler.java
+++ infod/src/main/java/com/next/infod/security/LoginSocialSucessHandler.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -17,11 +18,13 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class LoginSocialSucessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+    private static final String SENHA_PADRAO = "321";
     private final UsuarioService usuarioService;
 
     @Override
@@ -37,6 +40,12 @@ public class LoginSocialSucessHandler extends SavedRequestAwareAuthenticationSuc
 
         Usuario usuario = usuarioService.obterPorEmail(email);  //Metodo que vai buscar usuario por email
 
+
+        if(usuario == null ) {
+            usuario = cadastrarUsuarioNaBase(email);
+        }
+
+
         CustomAuthentication customAuthentication = new CustomAuthentication(usuario); //Modificamos pra nossa custom
 
         SecurityContextHolder.getContext().setAuthentication(authentication); //Adicionamos no contexto
@@ -45,6 +54,20 @@ public class LoginSocialSucessHandler extends SavedRequestAwareAuthenticationSuc
 
     }
 
+    private Usuario cadastrarUsuarioNaBase(String email) {
+        Usuario usuario;
+        usuario = new Usuario();
+        usuario.setEmail(email);
+        usuario.setLogin(obterLoginApartirEmail(email));
+        usuario.setSenha(SENHA_PADRAO);
+        usuario.setRoles(List.of("OPERADOR"));
+        usuarioService.create(usuario);
+        return usuario;
+    }
+
+    private String obterLoginApartirEmail(String email) {
+        return email.substring(0, email.indexOf("@"));
+    }
 
 
 }
