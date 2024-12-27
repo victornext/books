51cbd9fb4dcd44ea54a0fc988953ecd89fc7f379
victornext <victorbrs517@gmail.com>
2024-12-27 00:06:22 -0300

    Authorization Server

---
+++ infod/src/main/java/com/next/infod/controller/ClientController.java
@@ -0,0 +1,25 @@
+package com.next.infod.controller;
+
+
+import com.next.infod.model.Client;
+import com.next.infod.services.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("clients")
+@RequiredArgsConstructor
+public class ClientController {
+
+    private final ClientService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('GERENTE')")
+    public void salvar(@RequestBody Client client) {
+        service.salvar(client);
+    }
+}
