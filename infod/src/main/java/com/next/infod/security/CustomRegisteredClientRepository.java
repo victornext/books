3b0f0d0d60dcce5671fdbfebe6c3f75821c74456
victornext <victorbrs517@gmail.com>
2025-01-04 23:15:13 -0300

    registeredCleint customizado

--- infod/src/main/java/com/next/infod/security/CustomRegisteredClientRepository.java
+++ infod/src/main/java/com/next/infod/security/CustomRegisteredClientRepository.java
@@ -1,5 +1,9 @@
 package com.next.infod.security;
 
+import com.next.infod.services.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.stereotype.Component;
@@ -8,28 +12,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class CustomRegisteredClientRepository implements RegisteredClientRepository {
 
-    private final Map<String, RegisteredClient> registeredClients = new HashMap<>();
+    private final ClientService clientService;
 
     @Override
     public void save(RegisteredClient registeredClient) {
-        if (registeredClient == null) {
-            throw new IllegalArgumentException("RegisteredClient cannot be null");
-        }
-        registeredClients.put(registeredClient.getId(), registeredClient);
+
     }
 
     @Override
     public RegisteredClient findById(String id) {
-        return registeredClients.get(id);
+        return null;
     }
 
     @Override
     public RegisteredClient findByClientId(String clientId) {
-        return registeredClients.values().stream()
-                .filter(client -> client.getClientId().equals(clientId))
-                .findFirst()
-                .orElse(null);
+        var client = clientService.obterPorClientId(clientId);
+
+        if (client == null) {
+            return null;
+        }
+        return RegisteredClient.
+                withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectURI())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build();
     }
 }
