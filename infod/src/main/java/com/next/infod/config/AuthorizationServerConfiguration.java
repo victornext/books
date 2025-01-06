300bfec72a29f6497404557b3fef8232d203cbd8
victornext <victorbrs517@gmail.com>
2025-01-06 13:50:28 -0300

    AuthorizationSerer

--- infod/src/main/java/com/next/infod/config/AuthorizationServerConfiguration.java
+++ infod/src/main/java/com/next/infod/config/AuthorizationServerConfiguration.java
@@ -3,16 +3,50 @@ package com.next.infod.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.Duration;
 
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfiguration {
+
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http); //Com essa linha authorization server ja esta habilitado
+
+
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());  //Plugin do auth2 com token em mãos consegue descobrir quem gerou o token
+
+        http.oauth2ResourceServer(oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()));  //Verificaçõ se o token é valido
+
+        http.formLogin(configurer -> configurer.loginPage("/login")); //Pagina de login customizada
+
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder(10);
+    }
+
+
 
     @Bean
     public TokenSettings tokenSettings(){
