3309acb2370b3bf283fce5eb8037af6a2b63bd2e
victornext <victorbrs517@gmail.com>
2025-01-24 00:39:50 -0300

    code token

--- infod/src/main/java/com/next/infod/controller/LoginViewController.java
+++ infod/src/main/java/com/next/infod/controller/LoginViewController.java
@@ -4,6 +4,8 @@ import com.next.infod.security.CustomAuthentication; // Corrigido a importação
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -21,5 +23,11 @@ public class LoginViewController {
             System.out.println(customAuth.getUsuario());
         }
         return "Olá " + authentication.getName();
+    }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAuthorizationCode(@RequestParam("code") String code) {
+        return "Seu authorzation code " + code;
     }
 }
